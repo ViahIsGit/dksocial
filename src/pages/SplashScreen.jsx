4c9b6d673158aab407e4fe54d0ce9f24967c1ae9
BananaClickerTeam <thebananaaaproject@gmail.com>
2025-11-17 08:13:18 -0800

    Update SplashScreen.jsx

--- src/pages/SplashScreen.jsx
+++ src/pages/SplashScreen.jsx
@@ -3,7 +3,8 @@ import './SplashScreen.css';
 export default function SplashScreen() {
   return (
     <div className="splash-screen" role="img" aria-label="Tela inicial do DK Material">
-      <img src="/icon.png" alt="Logo DK" className="splash-logo" />
+      <img src="icon.png" alt="Logo DK" className="splash-logo" />
     </div>
   );
 }
+
